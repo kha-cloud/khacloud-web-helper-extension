9970874b704753a71cffa11382519119547439a2
Khalil ben zineb <khalil.benzineb@gmail.com>
2022-08-03 20:22:19 +0100

    08/03/22

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 var isKhacloudPage = true;
 chrome.runtime.onMessage.addListener(function(data) {
     // document.getElementById('url').value = data;
-    console.log('Message from iframe: '+data.id);
-    console.log(data);
-    console.log(window.khacloudIframeDetector);
-    if(!window.khacloudIframeDetector) console.log(window);
-    if(window.khacloudIframeDetector) window.$nuxt_bus.$emit("khacloud-iframe-detector", data);
-    if(window.khacloudIframeDetector) window.khacloudIframeDetector(data);
+    // console.log('Message from iframe: '+data.id);
+    // console.log(data);
+    // console.log(window.$nuxt_bus);
+    // if(!window.$nuxt_bus) console.log(window);
+    // window.$nuxt_bus.$emit("khacloud-iframe-detector", data);
+    window.postMessage({ type: "KHACLOUD_PAGE", ...data}, "*");
 });
