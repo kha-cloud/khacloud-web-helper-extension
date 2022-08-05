92f28bc65e95d5d515a5a237c0291a9972d18b3d
rafik mechria <rafikmechria@gmail.com>
2022-08-05 20:59:28 +0100

    Fix : allow x-frame protected websites in iframe

--- background.js
+++ background.js
@@ -9,25 +9,3 @@ chrome.runtime.onMessage.addListener(function(message, sender) {
       chrome.tabs.sendMessage(sender.tab.id, message.data);
   }
 });
-
-
-
-
-
-// // // Drops X-Frame-Options and Content-Security-Policy HTTP response headers, allowing all pages to be iframed
-
-// const HEADERS_TO_STRIP_LOWERCASE = [
-//   'content-security-policy',
-//   'x-frame-options',
-// ];
-
-// chrome.webRequest.onHeadersReceived.addListener(
-//   details => ({
-//     responseHeaders: details.responseHeaders.filter(header =>
-//         !HEADERS_TO_STRIP_LOWERCASE.includes(header.name.toLowerCase()))
-//   }),
-//   {
-//     urls: ['<all_urls>']
-//   },
-//   ['blocking', 'responseHeaders', 'extraHeaders']
-// );
